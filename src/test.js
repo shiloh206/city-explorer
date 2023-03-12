3e639cf21a5f07e0db55b624c6b4bc983688b398
Ahmed Mohamed <ahmed.mohamed57@outlook.com>
2023-03-12 14:33:45 -0700

    form works, maps work

---
+++ src/test.js
@@ -0,0 +1,63 @@
+// import react from "react";
+// import Button from 'react-bootstrap/Button';
+// import Form from 'react-bootstrap/Form';
+
+
+
+
+
+
+
+
+
+
+// <>
+// <h1>API Calls</h1>
+// <Form onSubmit={this.getCityData}>
+//   <Form.Group>
+//     <Form.Label as='form-label'>City Name:</Form.Label>
+//     <Form.Control type='text' placeholder='City Name' onInput={this.handleInput}></Form.Control>
+//     <Button type='submit'>Explore!</Button>
+//   </Form.Group>
+// </Form>
+
+// {
+//   this.state.error
+//     ? <Alert variant="warning">{this.state.errorMessage}</Alert>
+//     : <Container>
+//       <ListGroup as='list-group'>
+//         <ListGroup.Item>City: {this.state.cityData.display_name}</ListGroup.Item>
+//         <ListGroup.Item>Latitude: {this.state.cityData.lat}</ListGroup.Item>
+//         <ListGroup.Item>Longitude: {this.state.cityData.lon}</ListGroup.Item>
+//       </ListGroup>
+//       <Image src={this.state.cityMap}></Image>
+//     </Container>
+// }
+// <Weather 
+//   weatherData={this.state.weatherData}
+// />
+// </>
+// class SForm extends React.Component {
+//     render () {
+
+//         return(
+//             <>
+//             <h1>Explore  and have fun</h1>
+//             <Form onSubmit={this.props.handleInput}>
+//                 <Form.Label id='searchQuery'></Form.Label>
+//                 <Form.Control type="text" id='searchQuery' placeholder="Enter the city..." /><br/>
+
+//                 <Button variant="primary" type="submit">submit</Button>
+
+//             </Form>
+//             </>
+//         )
+
+
+//         };
+// };
+
+
+
+
+// export default SForm;
