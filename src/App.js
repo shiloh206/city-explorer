92e4ee0a1187b5090c97e23208c12211d9c582f0
Ahmed Mohamed <ahmed.mohamed57@outlook.com>
2023-03-12 17:14:15 -0700

    test push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import axios from 'axios';
+import React from 'react';
 import { Form, Button, Alert, Container, ListGroup, Image } from 'react-bootstrap';
  import Weather from './Weather';
 
@@ -69,7 +69,7 @@ class App extends React.Component {
 
     return (
       <>
-        <h1>API Calls</h1>
+        <h1>City Explore</h1>
         <Form onSubmit={this.getCityData}>
           <Form.Group>
             <Form.Label as='form-label'>City Name:</Form.Label>
@@ -98,119 +98,3 @@ class App extends React.Component {
 
 export default App;
 
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import axios from 'axios';
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       city: '',
-//       cityName: '',
-//       cityData: {},
-//       error: false,
-//       errorMessage: ''
-//     }
-//   }
-
-//   eventHandler = async (e) => {
-//     e.preventDefault();
-//     try {
-//       // axios is the library of code that we will use to make our requests
-//       let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_API_KEY}&q=${this.state.city}&format=json`
-//       // console.log(starWarsCharacters.data.results);
-//       //save it in state
-//       this.setState({
-//         cityData: cityDataFromAxios.data[0],
-//         cityMap: cityMap,
-//         error: false
-//       });
-//     } catch (error) {
-//       this.setState({
-//         error: true,
-//         errorMessage: `${error.message}`
-//       })
-//     }
-//   }
-//   handleGetWeather = async (lat, lon) => {
-//     try {
-//       let url = `${process.env.REACT_APP_SERVER}/weather?lat=${lat}&lon=${lon}`
-  
-//       let weatherDataFromAxios = await axios.get(url);
-  
-//       this.setState({
-//         weatherData: weatherDataFromAxios.data
-//       })
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-  
-//   citySubmit = async (e) => {
-//     e.preventDefault();
-//     // what city are we searching for — the one in state
-//     const { cityName } = this.state;
-  
-//     // get data from the Location IQ API
-//     try {
-//       let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_API_KEY}&q=${cityName}&format=json`);
-//       console.log(cityData.data[0]);
-//       // save that data in state
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-  
-
-//   render() {
-
-//     let starWarsListItems = this.state.starWarsData.map((swData, idx) => {
-//       // console.log(idx);
-//       // console.log(swData);
-//       return <li key={idx}>{swData.name}</li>
-//     })
-
-//     let mapURL = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=47.6038321,-122.330062&zoom=12`
-
-//     console.log(this.state.cityName);
-//     return (
-//        <>
-//         <h1>API Calls</h1>
-//         <Form onSubmit={this.getCityData}>
-//           <Form.Group>
-//             <Form.Label as='form-label'>City Name:</Form.Label>
-//             <Form.Control type='text' placeholder='City Name' onInput={this.handleInput}></Form.Control>
-//             <Button type='submit'>Explore!</Button>
-//           </Form.Group>
-//         </Form>
-
-//         {
-//           this.state.error
-//             ? <Alert variant="warning">{this.state.errorMessage}</Alert>
-//             : <Container>
-//               <ListGroup as='list-group'>
-//                 <ListGroup.Item>City: {this.state.cityData.display_name}</ListGroup.Item>
-//                 <ListGroup.Item>Latitude: {this.state.cityData.lat}</ListGroup.Item>
-//                 <ListGroup.Item>Longitude: {this.state.cityData.lon}</ListGroup.Item>
-//               </ListGroup>
-//               <Image src={this.state.cityMap}></Image>
-//             </Container>
-//         }
-//         <Weather 
-//           weatherData={this.state.weatherData}
-//         />
-//       </>
-//     );
-//   }
-// }
-
-// export default App;
